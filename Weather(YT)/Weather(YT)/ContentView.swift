e6b7e31994b7c908849d837b92399e34e14f0b79
Park Hyean Sue <108973610+psue04@users.noreply.github.com>
2022-09-26 11:02:34 +0900

    Add files via upload

---
+++ Weather(YT)/Weather(YT)/ContentView.swift
@@ -0,0 +1,101 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isNight = false
+    
+    var body: some View {
+        ZStack {
+            BackgroundView(topColor: isNight ? .black : .blue,
+                           bottomColor: isNight ? .gray : Color("lightBlue"))
+            
+            VStack {
+                CityTextView(cityName: "Cupertino, CA")
+                
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" :  "cloud.sun.fill", temperature: 76)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUES", imageName: "cloud.sun.fill", temperature: 74)
+                    
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                    
+                    WeatherDayView(dayOfWeek: "THURS", imageName: "wind.snow", temperature: 55)
+                    
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
+                    
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "snow", temperature: 25)
+                }
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                }
+                
+                Spacer()
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default)).foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original).resizable()
+                .aspectRatio(contentMode: .fit).frame(width: 40, height: 40)
+            
+            Text("\(temperature)°").font(.system(size: 28, weight: .medium)).foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName).font(.system(size: 32, weight: .medium, design: .default)).foregroundColor(.white).padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: imageName).renderingMode(.original).resizable().aspectRatio(contentMode: .fit).frame(width: 180, height: 180)
+            
+            Text("\(temperature)°").font(.system(size: 70, weight:.medium)).foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
---
+++ Weather(YT)/Weather(YT)/Weather_YT_App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_YT_App.swift
+//  Weather(YT)
+//
+//  Created by Park Hyean Sue on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct Weather_YT_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
