0fb6aebdc2ed2bff1a927104be25c607dd7c88fe
Park Hyean Sue <108973610+psue04@users.noreply.github.com>
2022-09-29 11:25:17 +0900

    Add files via upload
    Weather demon

---
+++ WeatherDemo-2/WeatherDemo-2/WeatherDemo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemo_2App.swift
+//  WeatherDemo-2
+//
+//  Created by Park Hyean Sue on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
